4dd989da4da65b0d1e14fbd891f90c6f69b68b8e
Dakhani Usman <dakhaniusman@gmail.com>
2024-10-03 21:11:51 +0530

    js updated

---
+++ Day-02/index.js
@@ -0,0 +1,39 @@
+// 1. Datatypes -> Data is every information
+// 1.1 String 
+// 1.2 Number
+// 1.3 Boolean
+// 1.4 Undefined 
+// 1.5 Null
+
+// 2. Variables -> containers to store data
+// 2.1 var 
+// 2.1 let 
+// 2.1 const 
+
+// 3. Operators 
+// 3.1 Arithmatic + - * /
+// 3.2 Conditional > < >= <=
+// 3.2 Comparison
+
+// 4. Conditional Statement -> statement that are executed as per conditions specified
+// 4.1 if  else if  else
+// if(){}  else if(){}  else{}
+
+var num1 = 15;
+var num2 = 20;
+
+console.log(num1 > num2);
+console.log(num1 == num2);
+console.log(num1 < num2);
+
+if(num1 > num2){
+    // false
+    console.log(num1, " is greater than ", num2);
+} else if (num1 == num2){
+    // false
+    console.log(num1, " is equal to ", num2);
+} else {
+    // true
+    console.log(num1, " is lesser than ", num2);
+}
+
--- Day-03/index.js
+++ Day-03/index.js
@@ -1,3 +1,25 @@
+// 1. Datatypes -> Data is every information
+// 1.1 String 
+// 1.2 Number
+// 1.3 Boolean
+// 1.4 Undefined 
+// 1.5 Null
+
+// 2. Variables -> containers to store data
+// 2.1 var 
+// 2.1 let 
+// 2.1 const 
+
+// 3. Operators 
+// 3.1 Arithmatic + - * /
+// 3.2 Conditional > < >= <=
+// 3.2 Comparison
+
+// 4. Conditional Statement -> statement that are executed as per conditions specified
+// 4.1 if  else if  else
+// 4.2 switch
+
+
 // 4.1 revision with another example
 // drving license
 // rule : age == 18 -> Learning license
