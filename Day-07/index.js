f987c1febc7bc785048a5ea1f85b39ad0f5541d0
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-10 22:35:31 +0530

    JavaScript Day 07
    DSA Practice question solving complex problems through loops

--- Day-07/index.js
+++ Day-07/index.js
@@ -37,3 +37,108 @@
 // 7.2 For loop
 // 7.3 DSA Questions for loop practice
 
+
+// every question in function
+
+// DSA Question 1
+// print numbers from 30 to 60 with range of 5
+
+var start = 30;
+var end = 60;
+var sequence = 5;
+
+function questionOne(start, end, sequence) {
+  console.log("DSA Question 1 - 30 to 60 with range of 5");
+
+  for (let i = start; i <= end; i += sequence) {
+    console.log(i);
+  }
+  // a function should return some value since this function is not returning anything it shows undefined
+}
+
+console.log(questionOne(start, end, sequence));
+console.log("-------------");
+
+// DSA Question 2
+// print numbers from 50 to 100 with range of 3 and count them
+
+var start = 50;
+var end = 100;
+sequence = 3;
+count = 0;
+
+function questionTwo(start, end, sequence) {
+    console.log("DSA Question 2 - print numbers from 50 to 100 with range of 3 and count them")
+  for (let i = start; i <= end; i += sequence) {
+    // console.log(i); // testing
+    count++;
+    console.log(count);
+  }
+  return count + " Final Count - Inside of loop";
+}
+
+console.log(questionTwo(start, end, sequence));
+
+console.log(count, "Final count - Outside of loop ");
+console.log("-------------");
+
+
+// DSA Question 3
+// print number form 10 to 50 with range 2
+// add 10 for each value
+// reutrn in array
+
+var start = 10;
+var end = 50;
+var sequence = 2;
+var array = []; // creating empty array 
+
+function questionThree(start, end, sequence){
+    console.log("DSA Question 3 - 10 to 50 with range 2 , add 10 , store in array")
+    // console.log(start, end, sequence);
+
+    for(var i = start ; i <= 50 ; i += sequence){
+        // console.log(i);  10 to 50 with range 2
+        // console.log(i + 10); value + 10
+
+        array.push(i + 10); // pushing each value in array
+    }
+    return array ; // final statment of function
+}
+
+console.log(questionThree(start, end, sequence))
+console.log("---------------")
+
+// DSA Question 4
+// print count from range 233 to 456, who are odd
+
+var start = 233;
+var end = 456;
+var sequence = 1;
+count = 0;
+
+
+function questionFour(start, end, sequence){
+    console.log("DSA Question 4 - print count from range 233 to 456, who are odd");
+
+    for (var i = start ; i <= end ; i += sequence){
+        // console.log(i); //  testing all
+        // if(i % 2 == 1){
+        //     console.log(i); // odd values
+        // }
+
+        if( i % 2 == 1){
+        count++;
+        }
+
+    }
+    console.log("final count is ", count)
+    return count + " number are odd number";
+
+}
+
+console.log(questionFour(start, end, sequence));
+console.log("---------------")
+
+
+console.log("////////// DAKHANI USMAN //////////")
