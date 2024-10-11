d4eaa5c9d07ed34247caa62c88650807ff9cec32
AvinashT31 <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-11 15:10:35 +0530

    js revision

--- Day-02/index.js
+++ Day-02/index.js
@@ -1,19 +1,19 @@
 // 1. Datatypes -> Data is every information
-// 1.1 String 
+// 1.1 String
 // 1.2 Number
 // 1.3 Boolean
-// 1.4 Undefined 
+// 1.4 Undefined
 // 1.5 Null
 
 // 2. Variables -> containers to store data
-// 2.1 var 
-// 2.1 let 
-// 2.1 const 
+// 2.1 var
+// 2.2 let
+// 2.3 const
 
-// 3. Operators 
+// 3. Operators
 // 3.1 Arithmatic + - * /
 // 3.2 Conditional > < >= <=
-// 3.2 Comparison
+// 3.3 Comparison
 
 // 4. Conditional Statement -> statement that are executed as per conditions specified
 // 4.1 if  else if  else
@@ -36,4 +36,3 @@ if(num1 > num2){
     // true
     console.log(num1, " is lesser than ", num2);
 }
-
