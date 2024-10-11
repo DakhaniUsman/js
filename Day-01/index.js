beda33300bebe2fd678f3900b1a19deec85d1527
AvinashT31 <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-11 12:19:39 +0530

    js practice

--- Day-01/index.js
+++ Day-01/index.js
@@ -1,28 +1,28 @@
-// JS is a scripting language which can be runned in computer through run time nvironment that is node js 
+// JS is a scripting language which can be runned in computer through run time nvironment that is node js
 
 // 1. Datatypes -> Data is every information
-// 1.1 String 
+// 1.1 String
 // 1.2 Number
 // 1.3 Boolean
-// 1.4 Undefined 
+// 1.4 Undefined
 // 1.5 Null
 
 // 2. Variables -> containers to store data
-// 2.1 var 
-// 2.1 let 
-// 2.1 const 
+// 2.1 var
+// 2.1 let
+// 2.1 const
 
-// 3. Operators 
+// 3. Operators
 // 3.1 Arithmatic + - * /
 // 3.2 Conditional > < >= <=
 // 3.2 Comparison
 
 // 1.1 String
 //  strings are written in "" '' ``
-//  everything written in these quotes are considered as string even numbers and special characters 
-//  "awdiz" 'awdiz' `awdiz` "123" '123' `123` "!@#$" '!@#$' `!@#$` all are string 
+//  everything written in these quotes are considered as string even numbers and special characters
+//  "awdiz" 'awdiz' `awdiz` "123" '123' `123` "!@#$" '!@#$' `!@#$` all are string
 
-//  Functions -> multiple lines of codes 
+//  Functions -> multiple lines of codes
 //  two types : inbuilt function  or function which we define
 
 // console.log() is and inbuilt funtion used to print the data within paranthesis
@@ -35,7 +35,7 @@ console.log("!@#" , typeof("!@#"))
 // string data types are shown in white colour in terminal
 
 
-// 1.2 Number 
+// 1.2 Number
 // number datatype dose not have any syntax
 // 123 12.3 12.0065 all are considered as number
 
@@ -59,16 +59,16 @@ console.log(false, typeof(false));
 // It has 3 concepts declaration assigning value and re assign
 
 
-// // 2.1 var 
+// // 2.1 var
 // var myName ; // Declaration
 // var myName = "Usman"; // Declaration + Assigning value
-// myName = "Dakhani Usman"; // Re assigning // can be re assigned 
-// var myName = "Dakhani Usman Fqir Mohammed"; // Re Declaration // can be re declared 
+// myName = "Dakhani Usman"; // Re assigning // can be re assigned
+// var myName = "Dakhani Usman Fqir Mohammed"; // Re Declaration // can be re declared
 // console.log(myName);
 
 
 
-// // 2.2 let 
+// // 2.2 let
 // // let myName ; // Declaration
 // let myName = "Usman"; // Declaration + Assigning value
 // myName = "Dakhani Usman"; // Re assigning
@@ -79,7 +79,7 @@ console.log(false, typeof(false));
 
 
 
-// // 2.3 const 
+// // 2.3 const
 // // var myName ; // Declaration
 // const myName = "Usman"; // Declaration + Assigning value
 // // myName = "Dakhani Usman"; // Re assigning // cannot be re assigned
@@ -96,10 +96,10 @@ console.log(false, typeof(false));
 // hoisting           true             false           false
 
 
-// 3. Operators 
+// 3. Operators
 
-// 3.1 Arithmatic 
- 
+// 3.1 Arithmatic
+
 console.log( 10 + 20);   // Add
 console.log( 40 - 20);   // Substract
 console.log( 10 * 2);    // Multiply
@@ -109,11 +109,19 @@ console.log( 100 / 25);  // Divide
 
 console.log( 10 > 20);   // Greater than
 console.log( 40 < 20);   // Less Than
-console.log( 10 >= 8);    // Greater than equal to 
+console.log( 10 >= 8);    // Greater than equal to
 console.log( 100 <= 25);  // Less than equal to
 
 // 3.3 Comparison
 
 console.log(10 == 10 ) // equal to only checks data // true
 console.log("10" == 10 ) // equal to only checks data // true which is wrong string != number
-console.log("10" === 10 ) // strict equal to checks data and data type 
+console.log("10" === 10 ) // strict equal to checks data and data type
+
+
+// // Hoisting
+// // var x; // Declaration is hoisted to the top but not initialization
+// console.log("before declaration ",x);  // undefined
+
+// x = 10;
+// console.log("after declaration ",x);   // after declaration 10
