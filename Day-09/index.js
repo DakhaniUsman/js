02250cfb3dd957a6d741368f0e510f2245aed0de
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-13 16:45:55 +0530

    js day 9

--- Day-09/index.js
+++ Day-09/index.js
@@ -41,3 +41,108 @@
 // 7.5 Reverse for loop
 // 7.6 Array with for loops
 // 7.7 While Loop
+
+//  while loop is better than for as we can control the sequence within the  loop itself
+
+// syntax
+//  var start ;  // declare
+// while ( end ) {   // condition
+//  code ;
+//  sequence
+// }
+
+// Question 1 - print number from 1 - 10 by using while loop
+
+console.log("Question 1 - print number from 1 - 10 by using while loop");
+
+var i = 1; // start
+while (i < 11) {
+  // end
+  console.log(i);
+  i++; // sequence u can set sequence as per you requirement
+}
+
+// // iteration -1
+// i < 11 --> 1 < 11 --> true
+// console.log(i);   --> 1
+// i++;              --> 2
+
+// iteration -2
+// i < 11 --> 2 < 11 --> true
+// console.log(i);   --> 2
+// i++;              --> 3
+
+// ......
+
+// iteration -10
+// i < 11 --> 10 < 11 --> true
+// console.log(i);   --> 10
+// i++;              --> 11
+
+// iteration -11
+// i < 11 --> 11 < 11 --> false
+// loop end run time goes on next piece of code
+
+console.log("----------------------");
+
+// Question 2 - print numbers from 10 - 1 using while loop
+// Reverse while loop
+
+console.log("Question 2 - print numbers from 10 - 1 using while loop");
+
+var i = 10;
+
+while (i > 0) {
+  console.log(i);
+
+  i--; // decreasing the value of i by 1 at each itteration
+}
+console.log("----------------------");
+
+// Question 3 - print count of numbers from given array by using while loop, who are even
+
+console.log(
+  "Question 3 - print count of numbers from given array by using while loop, who are even"
+);
+
+var array = [1, 20, 25, 24, 70, 80, 55, 77];
+
+// console.log(array.length);
+// console.log(array[0]);
+
+// print value from array   --> completeed
+// only even numbers        --> completed
+// count of even numbers    --> completed
+
+var i = 0;
+count = 0;
+
+while (i < array.length) {
+  // console.log(array[i]);  // all values from array
+  if (array[i] % 2 == 0) {
+    // console.log(array[i]); // only even numbers
+    count++;
+  }
+
+  i++;
+}
+console.log(array);
+console.log(count + " numbers are even from the given array");
+console.log("----------------------");
+
+// Not Equal to operator (!=)
+
+console.log("Not equal to operator != and !==");
+
+console.log(1 == 1); // true  we check if left side is equal to right side
+console.log(1 == 2); // true
+
+console.log(1 != 1); // false  we check if left side is not equal to right side
+console.log(1 != 2); // true
+
+console.log(1 != "1"); // false  we check if left side is not equal to right side
+console.log(1 !== "1"); // true
+
+console.log("----------------------");
+
+console.log("////////// DAKHANI USMAN //////////");
