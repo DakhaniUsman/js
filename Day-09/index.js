31650fe8669c2e3c6318f8f57eb85b46cb4fd4fa
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-16 00:21:24 +0530

    JS Day 09

--- Day-09/index.js
+++ Day-09/index.js
@@ -148,14 +148,3 @@ console.log("----------------------");
 console.log("////////// DAKHANI USMAN //////////");
 
 
-var array = [10,20,30,40,50]
-
-var i = 0;
-
-while(i < array.length){
-  if (array[i] != 30 && array[i] != 10){
-    console.log(array[i])
-    
-  }
-  i++;
-}
