8a91f1c7246d03b5f95825955c8c5ef5bf67cf32
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-19 13:01:36 +0530

    JS Day 11

--- Day-11/index.js
+++ Day-11/index.js
@@ -46,19 +46,31 @@
 // 8.1 Linear Search
 // 8.2 Biniarry Search
 
-// linear seach means searching each elemen one after other linearly
+// linear seach means searching each element one after other linearly
+
+// Question given an array find the index of target
 
 var array = [9, 8, 7, 6, 5, 4, 3, 2, 1];
 var target = 5;
-
+// output 4
 console.log("Linear Search Method");
 
 for (var i = 0; i < array.length; i++) {
   //   console.log(i); // printing index values
   if (array[i] === target) {
+    // array[i] 5 == 5 => true
     console.log(i, "is the index value of target");
   }
 }
+
+// var i = 0;
+// while(i < array.length){
+//   if(array[i] === target){
+//     console.log(i , " is the index value of target.")
+//   }
+//   i++;
+// }
+
 console.log("----------------");
 
 // Question we have to find the index of target using Binary Search method
@@ -68,16 +80,72 @@ console.log("----------------");
 // we split the array into two part
 // check three condtions
 // middle = (array.length - 1) / 2
-var array = [1, 2, 3, 4,      5,      6, 7, 8, 9];
-var target = 8;
 
-console.log(array.length);
-console.log((array.length - 1) / 2);
-console.log(Math.floor((array.length - 1) / 2));
+console.log("Binary Seach Method");
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var target = 1;
 
-var middleIndex = Math.floor((array.length - 1) / 2);
-// console.log(array[middleIndex]);
 
-if ( array[middleIndex] === target ){
-    console.log("We have found the target it's on " , middleIndex , " index position");
+console.log(array);
+console.log(target, " is our target element")
+// Linear Search => 8 itterations
+// how to do it in less iterations ???
+
+// console.log(array.length);     // total length of array
+// console.log((array.length - 1) / 2);     // half length
+// console.log(Math.floor((array.length - 1) / 2));   // rounding off half length
+
+var firstIndex = 0; // middle index
+var middleIndex = Math.floor((array.length - 1) / 2); // middle index
+var lastIndex = array.length - 1; // middle index
+// console.log(array[middleIndex]); // middle value of array
+
+// console.log(firstIndex, array[firstIndex]); //  value at index 0
+// console.log(middleIndex, array[middleIndex]); // value at index  4
+// console.log(lastIndex, array[lastIndex]); // value at last index 8
+
+if (array[middleIndex] === target) {
+  // 5 == target
+  console.log("1st condition is ", array[middleIndex] == target);
+
+  console.log("We have found the target at", middleIndex, "index value");
+} else if (array[middleIndex] < target) {
+  // go towards right
+  //  5 < target  => true
+  console.log("2nd condition is ", array[middleIndex] < target);
+
+  let i = middleIndex; // start from middleIndex
+
+  while (i <= lastIndex) {
+    // 5 < 8
+
+    if (array[i] === target) {
+      console.log("We have found the target at ", i, " index position");
+    }
+
+    i++; // go till last index
+  }
+} else {
+  // go towards left
+  // 5 > target
+  console.log("3rd condition is ", array[middleIndex] > target);
+
+  let i = middleIndex;
+  // console.log(i);
+
+  while (i >= firstIndex) {
+    // 5 > 0
+
+    if (array[i] === target) {
+      console.log("We have found the target at ", i, " index position");
+    }
+
+    i--; // go till first index
+  }
 }
+
+
+
+console.log("----------------------");
+
+console.log("////////// DAKHANI USMAN //////////");
