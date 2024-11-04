f0d6f5c7ce8007af5326f071c997a7ff75c062f0
AvinashT31 <115381340+DakhaniUsman@users.noreply.github.com>
2024-11-04 14:43:39 +0530

    js revision

--- Day-09/index.js
+++ Day-09/index.js
@@ -148,3 +148,19 @@ console.log("----------------------");
 console.log("////////// DAKHANI USMAN //////////");
 
 
+// TestQuestion print even numbers from an array using while loop
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var i = 0;
+
+while (i < array.length) {
+
+  if (array[i] % 2 == 0) {
+    console.log(array[i]);
+  }
+
+  i++;
+
+}
+
