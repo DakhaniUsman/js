c257780ad95a2a1e32dd5439025d11af078723b0
AvinashT31 <115381340+DakhaniUsman@users.noreply.github.com>
2024-11-23 11:39:38 +0530

    practice

---
+++ practice/try.js
@@ -0,0 +1,48 @@
+
+
+var input = [2, 7, 11, 15];
+var target = 9;
+
+var input = [3, 2, 4];
+var target = 6;
+
+var input = [3, 3];
+var target = 6;
+
+function Question1() {
+    for (let i = 0; i < input.length; i++) {
+        // console.log(input[i]);
+
+        for (let j = i + 1; j < input.length; j++) {
+
+            if (input[i] + input[j] === target) {
+                console.log(i, j)
+            }
+
+        }
+    }
+}
+
+console.log(Question1());
+
+
+//
+
+var input = [3, 2, 2, 3];
+var element = 3;
+var output = [];
+function removeElement(input, element) {
+
+    for (let i = 0; i < input.length; i++) {
+
+        if (input[i] !== element) {
+            output.push(input[i]);
+        }
+
+
+    }
+
+    return output;
+}
+
+console.log(removeElement(input, element));
